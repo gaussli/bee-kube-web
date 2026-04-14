af818cba35a5a6cb12a66e537e8f1562831b48c9
gaussli <gauss_li@163.com>
2026-04-15 01:59:21 +0800

    eslint

---
+++ eslint.config.js
@@ -0,0 +1,83 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import * as parserVue from 'vue-eslint-parser'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsparser from '@typescript-eslint/parser'
+import prettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default [
+  {
+    ignores: ['node_modules', 'dist', '*.local', '.env*']
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*'],
+    languageOptions: {
+      globals: {
+        console: 'readonly',
+        document: 'readonly',
+        localStorage: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        Date: 'readonly',
+        JSON: 'readonly',
+        Math: 'readonly',
+        Promise: 'readonly',
+        Object: 'readonly',
+        __dirname: 'readonly'
+      }
+    },
+    rules: {
+      'no-undef': 'error',
+      'no-redeclare': 'off',
+      'no-unused-vars': 'off'
+    }
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: parserVue,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        parser: tsparser
+      }
+    },
+    plugins: {
+      vue: pluginVue
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/no-v-html': 'off'
+    }
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', 'vite.config.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-redeclare': 'off',
+      'no-prototype-builtins': 'off'
+    }
+  },
+  eslintConfigPrettier,
+  {
+    plugins: {
+      prettier
+    },
+    rules: {
+      'prettier/prettier': 'error'
+    }
+  }
+]
